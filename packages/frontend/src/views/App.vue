4e199e8255399c7044dec15b27545c1df56f988c
HAHWUL <hahwul@gmail.com>
2025-06-29 13:48:43 +0900

    Reload OAST providers when tab becomes active

--- packages/frontend/src/views/App.vue
+++ packages/frontend/src/views/App.vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import TabPanel from "primevue/tabpanel";
 import TabView from "primevue/tabview";
 import Toast from "primevue/toast";
 
 import { GuideTab, OastTab, ProvidersTab } from "@/components/tabs";
+
+const activeTabIndex = ref(0);
 </script>
 
 <template>
-  <TabView class="h-full">
-    <TabPanel header="OAST">
-      <OastTab />
-    </TabPanel>
-    <TabPanel header="Providers">
-      <ProvidersTab />
-    </TabPanel>
-    <TabPanel header="Guide">
-      <GuideTab />
-    </TabPanel>
-  </TabView>
-  <Toast />
+    <TabView v-model:activeIndex="activeTabIndex" class="h-full">
+        <TabPanel header="OAST">
+            <OastTab :active="activeTabIndex === 0" />
+        </TabPanel>
+        <TabPanel header="Providers">
+            <ProvidersTab />
+        </TabPanel>
+        <TabPanel header="Guide">
+            <GuideTab />
+        </TabPanel>
+    </TabView>
+    <Toast />
 </template>
